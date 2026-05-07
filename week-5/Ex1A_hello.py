267b0519e49c36bc98d50cc531793ea72bc6639f
Paola Martinez <pmartinez@my.yearupunited.org>
2026-05-07 11:57:35 -0500

    python 1A file

--- week-5/Ex1A_hello.py
+++ week-5/Ex1A_hello.py
@@ -1,1 +1,21 @@
-print('Hello world')
+print("Hello world!")
+
+message = "hello world!"
+print(message)
+# "Hello world prints twice because there are two print statements"
+
+# Displaying dollars and cents 
+dollars = 3 
+cents = .50
+print(dollars + cents)
+#3.5 float 
+cents = cents + .25
+print(dollars + cents)
+3.75
+
+d_str = "3 dollars"
+c_str = "50 cents"
+print(d_str + " " + c_str)
+#3 dollars 50 cents
+
+
