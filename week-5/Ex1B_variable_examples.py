888ea90464d7087f0e031177c4efe41c9d4f1a45
Paola Martinez <pmartinez@my.yearupunited.org>
2026-05-07 14:32:29 -0500

    Python 1B file

---
+++ week-5/Ex1B_variable_examples.py
@@ -0,0 +1,64 @@
+Customer_ID = 675
+Customers_name = "Bill Burn"
+Customer_gender = "male"
+Customer_date_of_birth = 7_24_1995
+Drivers_license_number = 321298
+Auto_policy_number = 219468
+
+#I wrote numbers for the IDs and numbers policies, a string of firt name and last name for name and the birthday, I underscored after each number to differentiate between the day month and year. Not sure if there is a better way to do that.
+
+my_name = "Paola Martinez"
+city = "Chicago"
+state = "Iliinois"
+
+
+#1a) What is the full list of reserved words that can't be used for variable names?
+#False	def	if	raise
+None	del	import	return
+#True	elif	in	try
+#and	else	is	while
+#as	except	lambda	with
+#assert	finally	nonlocal	yield
+#break	for	not
+#class	form	or
+#continue	global	pass
+
+#1b) 
+#if - A conditional statement that runs a block of code only when a condition is true 
+
+#ex: 
+#age = 18 
+#if age >= 18:
+#print("You are an adult")
+
+#for - a loop that repeats a blcok of code for each item in a sequence . 
+#it goes through a list, range, or other collection one item at a time 
+
+#ex:
+#for number in [1,2,3]:
+#print(number)
+#prints 1, then 2, then 3 
+
+#return - Exits a function and sends a value back to whoever called it.
+#used inside functions to output a result.
+
+#ex: def add(a, b): 
+# return a +b 
+#result = add(3, 5) 
+#result = 8 
+
+#True/False - Boolean values that represent yes/no or on/off
+# - the only two possible outcomes of a condition 
+#used in conditions and comparisons constantly.
+
+#ex: is_raining = True 
+#if is_raining: 
+    #print("Bring an umbrella")
+
+#def - short for "define" - used to create a reusable block of 
+#code called a function. 
+#Anything indented under def belongs to that function 
+
+#ex: def greet(name)
+#print("Hello " = name)
+#greet9"Paola") # prints: Hello Paola 
